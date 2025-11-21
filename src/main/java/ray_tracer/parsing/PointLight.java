7fc106b8f82f3fba2cfb35697de2f814be61d5d1
Amal65045 <amal.derouich@etu.imt-nord-europe.fr>
2025-11-21 15:59:40 +0100

    Add  comment for PointLight class
    Ajout d'un commentaire Javadoc pour la classe PointLight.

--- src/main/java/ray_tracer/parsing/PointLight.java
+++ src/main/java/ray_tracer/parsing/PointLight.java
@@ -3,7 +3,12 @@ package ray_tracer.parsing;
 import ray_tracer.geometry.Point;
 import ray_tracer.imaging.Color;
 
+/**
+  Représente une lumière ponctuelle, c'est-à-dire une lumière située à un point précis dans la scène.
+  Elle hérite de AbstractLight, donc elle possède déjà une couleur.
+ */
 public class PointLight extends AbstractLight {
+    // Position de la lumière dans l'espace 3D
     private Point position;
 
     public PointLight(Point position, Color color) {
