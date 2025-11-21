6a2866a392fb4462754f19041141140df79ad22b
Amal65045 <amal.derouich@etu.imt-nord-europe.fr>
2025-11-21 16:05:30 +0100

    Add  comments to AbstractLight class
    Added documentation for the AbstractLight class.

--- src/main/java/ray_tracer/parsing/AbstractLight.java
+++ src/main/java/ray_tracer/parsing/AbstractLight.java
@@ -1,7 +1,11 @@
 package ray_tracer.parsing;
 
 import ray_tracer.imaging.Color;
-
+/**
+Classe abstraite représentant une lumière.
+Elle définit uniquement une couleur, car toutes les lumières ont au minimum une couleur.
+Les classes qui héritent (PointLight, DirectionalLight) l'utiliseront.
+ */
 public abstract class AbstractLight {
     private Color color;
 
