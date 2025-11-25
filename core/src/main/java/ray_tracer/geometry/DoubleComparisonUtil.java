3f2f5e52e0f0021b556d4a3f2aeddb6279860f65
Maxence CHOSIEL <maxencechoisel@gmail.com>
2025-11-25 11:13:13 +0100

    clean

--- core/src/main/java/ray_tracer/geometry/DoubleComparisonUtil.java
+++
@@ -1,9 +0,0 @@
-package ray_tracer.geometry;
-
-public class DoubleComparisonUtil {
-    private static final double EPSILON = 1e-9;
-
-    public static boolean approximatelyEqual(double num1, double num2) {
-        return Math.abs(num1 - num2) < EPSILON;
-    }
-}
